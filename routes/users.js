63b19fae81c94ab26b9902b49ec858edb9fba9b5
Neeraj <yneeraj082r@gmail.com>
2023-12-30 14:48:56 +0530

    second commit

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-// mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
-mongoose.connect("mongodb+srv://yneeraj082:neeraj@cluster0.gf8xdxr.mongodb.net/")
+mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
 
 const userSchema = mongoose.Schema({
   username:String,
