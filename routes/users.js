a4e1532bceadecc3d8b3f81501b19eaed94ca35e
Neeraj <yneeraj082r@gmail.com>
2024-01-01 13:52:37 +0530

    adding user2 to cluster

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,18 @@ const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
 // mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
-mongoose.connect("mongodb+srv://yneeraj082r:9mtXq8qyIfaZUVeA@instacluster.yldfdt6.mongodb.net/mongodb?retryWrites=true&w=majority")
+// mongoose.connect("mongodb+srv://yneeraj082r:9mtXq8qyIfaZUVeA@instacluster.yldfdt6.mongodb.net/mongodb?retryWrites=true&w=majority")
+
+const Connection = async ()=>{
+  const URL = `mongodb+srv://user2:123@instacluster.yldfdt6.mongodb.net/?retryWrites=true&w=majority`;
+  try{
+    await mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true});
+    console.log("Database connected succesfully");
+  }catch(error){
+    console.log("Error",error);
+  }
+}
+Connection();
 
 const userSchema = mongoose.Schema({
   username:String,
